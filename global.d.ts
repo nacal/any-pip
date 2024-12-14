fc3933d4e41d82f8e8b6fb244f39fb3f9aa637c3
nacal <nacal@pepabo.com>
2024-12-15 01:55:08 +0900

    declare global

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,17 @@
-interface DocumentPictureInPicture {
-  requestWindow(options: { width: number; height: number }): Promise<Window>
-}
-
 declare global {
-  interface Window {
-    documentPictureInPicture: DocumentPictureInPicture
+  interface DocumentPictureInPictureOptions {
+    width?: number
+    height?: number
+    disallowReturnToOpener?: boolean
+    preferInitialWindowPlacement?: boolean
   }
+  interface DocumentPictureInPicture {
+    requestWindow: (
+      options?: DocumentPictureInPictureOptions
+    ) => Promise<Window>
+  }
+
+  var documentPictureInPicture: DocumentPictureInPicture
 }
 
 export global {}
