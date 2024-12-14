54ccaccfe90287657440818f310c1a5fefba4375
nacal <nacal@pepabo.com>
2024-12-14 21:36:08 +0900

    fix

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,7 @@
-// APIのインターフェース定義を追加
 interface DocumentPictureInPicture {
   requestWindow(options: { width: number; height: number }): Promise<Window>
 }
 
-// windowオブジェクトの拡張
 declare global {
   interface Window {
     documentPictureInPicture: DocumentPictureInPicture
